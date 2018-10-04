fcb07c51a0aaf938e78fe8f53897124828c1aed1
Nathan Boutté <nathan.boutte@viacesi.fr>
2018-10-04 11:59:05 +0200

    adding Iban element

---
+++ src/Iban.vue
@@ -0,0 +1,33 @@
+<template>
+  <stripe-element
+    ref='element'
+    type='iban'
+    :stripe='stripe'
+    :value='value'
+    :options='defaultOptions'
+    @blur='$emit("blur")'
+    @focus='$emit("focus")'
+    @change='$emit("change", $event)'
+  />
+</template>
+
+<script>
+import props from './props'
+import StripeElement from './StripeElement'
+
+export default {
+  props,
+  computed: {
+    defaultOptions() {
+      return Object.assign({ supportedCountries: ['SEPA'] }, this.options || {})
+    }
+  },
+  components: { StripeElement },
+  methods: {
+    blur () { this.$refs.element.blur() },
+    clear () { this.$refs.element.clear() },
+    focus () { this.$refs.element.focus() },
+    update (options) { this.$refs.element.update(options) }
+  }
+}
+</script>
